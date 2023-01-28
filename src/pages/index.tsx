f2054676000a1d16c5594a6ca27c1c1860376840
Axel Almquist <axel@almquist.io>
2023-01-28 14:40:08 +0100

    init + discord auth

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { api } from "../utils/api";
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+        <title className="flex text-black">NeoDraw</title>
+        <main>
+        <div>
+        <AuthShowcase />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div>
+      <p className="text-black">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
